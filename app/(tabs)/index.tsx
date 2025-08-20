011077d9ff68f14d01469c935abd3b551d76c46c
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-19 22:45:03 -0600

    install "react-native-safe-area-context" package and update index.tsx to test nativewind setup working or not

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
