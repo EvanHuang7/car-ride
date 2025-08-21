72eba18b6acdf080999055d87ebb7251c666da87
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-21 16:10:16 -0600

    WIP: use Swiper in onboarding page to display swiper data title first

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,16 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -14,6 +21,26 @@ const Onboarding = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id}>
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
