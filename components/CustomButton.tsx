55f3c50f0f4e464ae9e5db9af3ebef8e3a1973fb
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-21 17:44:03 -0600

    add styling for CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,35 @@
+import { ButtonProps } from "@/types/type";
 import { Text, TouchableOpacity } from "react-native";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286FF]";
+  }
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white";
+  }
+};
 
 const CustomButton = ({
   onPress,
@@ -9,11 +40,17 @@ const CustomButton = ({
   IconRight,
   className,
   ...props
-}) => {
+}: ButtonProps) => {
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity
+      onPress={onPress}
+      className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      {...props}
+    >
       {IconLeft && <IconLeft />}
-      <Text>{title}</Text>
+      <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+        {title}
+      </Text>
       {IconRight && <IconRight />}
     </TouchableOpacity>
   );
