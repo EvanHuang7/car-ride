51a60b2171e6303183877a69ffa9cefca150022b
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-21 15:37:31 -0600

    add skip button to redirect user to sign up page in welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,19 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 const Onboarding = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Onboarding</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
