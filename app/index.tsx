91dd25f5333aa3f85eec3bcc61f5bd49682cae74
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-23 16:46:24 -0600

    protect routes by checking if user signedIn in root index.tsx file

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,15 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
+
 import "./global.css";
 
 const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
+
   return <Redirect href="/(auth)/welcome" />;
 };
 
