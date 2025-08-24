ec9491c00588f7399859492a9c851a6415fe492f
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-23 21:10:51 -0600

    fix the broken styling of Tabs in root layout

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -12,10 +12,15 @@ const TabIcon = ({
   focused: boolean;
 }) => (
   <View
-    className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-general-300" : ""}`}
+    className={`items-center justify-center rounded-full ${
+      focused ? "bg-general-300" : ""
+    }`}
+    style={{ width: 56, height: 56 }}
   >
     <View
-      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+      className={`rounded-full w-12 h-12 items-center justify-center ${
+        focused ? "bg-general-400" : ""
+      }`}
     >
       <Image
         source={source}
@@ -38,16 +43,14 @@ export default function Layout() {
         tabBarStyle: {
           backgroundColor: "#333333",
           borderRadius: 50,
-          paddingBottom: 0, // ios only
-          overflow: "hidden",
           marginHorizontal: 20,
           marginBottom: 20,
-          height: 78,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexDirection: "row",
+          height: 88, // a bit taller for proper centering
           position: "absolute",
+          flexDirection: "row",
+          justifyContent: "space-around", // evenly space icons
+          alignItems: "center", // keep icons vertically centered
+          overflow: "hidden",
         },
       }}
     >
