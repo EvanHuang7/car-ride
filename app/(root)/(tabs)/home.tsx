604e75d891b8b31d5df195a72bc2760c4f88dc54
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-24 17:50:47 -0600

    display different content in home page based on if user logs in

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,27 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../../global.css";
 
 const Home = () => {
+  const { user } = useUser();
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Home</Text>
+      <SignedIn>
+        <Text className="text-xl font-bold text-blue-500">
+          Hello 👋 {user?.emailAddresses[0].emailAddress}
+        </Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </SafeAreaView>
   );
 };
