240c768fc0c15297d4b3d4b9988a3bacc296e3c1
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-24 20:24:25 -0600

    WIP: add orginal address and destination address info. improve the styling in RideCar component

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,3 +1,4 @@
+import { icons } from "@/constants";
 import { Ride } from "@/types/type";
 import { Image, Text, View } from "react-native";
 
@@ -12,11 +13,35 @@ const RideCard = ({ ride }: { ride: Ride }) => {
             }}
             className="w-[80px] h-[90px] rounded-lg"
           />
+
+          <View className="flex flex-col mx-5 gap-y-5 flex-1">
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.to} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {ride.origin_address}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.point} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {ride.destination_address}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="flex flex-col w-full mt-5 bg-general-500 rounded-lg p-3 items-start justify-center">
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Driver
+            </Text>
+            <Text className="text-md font-JakartaBold">
+              {ride.driver.first_name} {ride.driver.last_name}
+            </Text>
+          </View>
         </View>
       </View>
-      <Text className="text-md font-JakartaBold">
-        {ride.driver.first_name} {ride.driver.last_name}
-      </Text>
     </View>
   );
 };
