9a50ecaa815169797f996d1db219500064372dfa
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-27 13:06:20 -0600

    add empty data state in home page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,8 @@
 import RideCard from "@/components/RideCard";
+import { images } from "@/constants";
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { FlatList, Text } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../../global.css";
@@ -115,12 +116,37 @@ const recentRides = [
 
 const Home = () => {
   const { user } = useUser();
+  const loading = false;
+
   return (
     <SafeAreaView>
       <SignedIn>
         <FlatList
-          data={recentRides?.slice(0, 5)}
+          data={[]}
           renderItem={({ item }) => <RideCard ride={item} />}
+          keyExtractor={(item, index) => index.toString()}
+          className="px-5"
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+          ListEmptyComponent={() => (
+            <View className="flex flex-col items-center justify-center">
+              {!loading ? (
+                <>
+                  <Image
+                    source={images.noResult}
+                    className="w-40 h-40"
+                    alt="No recent rides found"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-sm">No recent rides found</Text>
+                </>
+              ) : (
+                <ActivityIndicator size="small" color="#000" />
+              )}
+            </View>
+          )}
         ></FlatList>
       </SignedIn>
       <SignedOut>
