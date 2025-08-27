fc7c4b725df4f30341c32e9cdad8323c2149ab5e
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-27 14:41:01 -0600

    add LocationStore interface in type.d.ts

--- types/type.d.ts
+++ types/type.d.ts
@@ -40,3 +40,30 @@ declare interface Ride {
     car_seats: number;
   };
 }
+
+declare interface LocationStore {
+  userLatitude: number | null;
+  userLongitude: number | null;
+  userAddress: string | null;
+  destinationLatitude: number | null;
+  destinationLongitude: number | null;
+  destinationAddress: string | null;
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+}
