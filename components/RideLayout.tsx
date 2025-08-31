dc265bb5ec8bca4fdd5e41ad1b4dcbb671d050ef
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-30 21:14:54 -0600

    install "@gorhom/bottom-sheet" package and use it in RideLayOut component.

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -1,18 +1,22 @@
+import Map from "@/components/Map";
+import { icons } from "@/constants";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { router } from "expo-router";
-import React from "react";
+import React, { useRef } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import Map from "@/components/Map";
-import { icons } from "@/constants";
-
 const RideLayout = ({
   title,
+  snapPoints,
+  children,
 }: {
   title: string;
   snapPoints?: string[];
   children: React.ReactNode;
 }) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
     <GestureHandlerRootView className="flex-1">
       <View className="flex-1 bg-white">
@@ -35,7 +39,20 @@ const RideLayout = ({
           <Map />
         </View>
 
-        {/* TODO: Finish the rest of this component before using it */}
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={snapPoints || ["40%", "85%"]}
+          index={0}
+        >
+          <BottomSheetScrollView
+            style={{
+              flex: 1,
+              padding: 20,
+            }}
+          >
+            {children}
+          </BottomSheetScrollView>
+        </BottomSheet>
       </View>
     </GestureHandlerRootView>
   );
