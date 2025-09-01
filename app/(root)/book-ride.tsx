7469d6f770e093a4db9a1bc025fe70693cd6c875
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-31 22:07:10 -0600

    remove unnecessary fetching stripe publishable key from server logic since it's stored in client side

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -4,7 +4,6 @@ import { icons } from "@/constants";
 import { formatTime } from "@/lib/utils";
 import { useDriverStore, useLocationStore } from "@/store";
 import { StripeProvider } from "@stripe/stripe-react-native";
-import { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 
 const BookRide = () => {
@@ -16,20 +15,9 @@ const BookRide = () => {
     (driver) => +driver.id === selectedDriver
   )[0];
 
-  const [publishableKey, setPublishableKey] = useState("");
-
-  const fetchPublishableKey = async () => {
-    const key = await fetchKey(); // fetch key from your server here
-    setPublishableKey(key);
-  };
-
-  useEffect(() => {
-    fetchPublishableKey();
-  }, []);
-
   return (
     <StripeProvider
-      publishableKey={publishableKey}
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
       merchantIdentifier="merchant.identifier" // required for Apple Pay
       urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
     >
