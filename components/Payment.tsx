b1e942baef69dc965ca597a7bbf0673b67e43da3
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-01 18:52:15 -0600

    change to displaying a success modal instead of simple alert after user finish payment successfully

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,11 +1,14 @@
 import CustomButton from "@/components/CustomButton";
+import { images } from "@/constants";
 import { fetchAPI } from "@/lib/fetch";
 import { useLocationStore } from "@/store";
 import { PaymentProps } from "@/types/type";
 import { useAuth } from "@clerk/clerk-expo";
 import { useStripe } from "@stripe/stripe-react-native";
+import { router } from "expo-router";
 import React, { useState } from "react";
-import { Alert } from "react-native";
+import { Alert, Image, Text, View } from "react-native";
+import { ReactNativeModal } from "react-native-modal";
 
 const Payment = ({
   fullName,
@@ -84,7 +87,6 @@ const Payment = ({
     } else {
       // If user finished payment successfully after Strip payment sheet modal opened
       setSuccess(true);
-      Alert.alert("Success", "🎉 Your payment was successful!");
       // NOTE: In real industry work, the best practice is to listen to
       // Strip webhook event instead of executing the callback function.
       await fetchAPI("/(api)/ride/create", {
@@ -116,6 +118,33 @@ const Payment = ({
         className="my-10"
         onPress={openPaymentSheet}
       />
+
+      <ReactNativeModal
+        isVisible={success}
+        onBackdropPress={() => setSuccess(false)}
+      >
+        <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
+          <Image source={images.check} className="w-28 h-28 mt-5" />
+
+          <Text className="text-2xl text-center font-JakartaBold mt-5">
+            Booking placed successfully 🎉
+          </Text>
+
+          <Text className="text-md text-general-200 font-JakartaRegular text-center mt-3">
+            Thank you for your booking. Your reservation has been successfully
+            placed. Please proceed with your trip.
+          </Text>
+
+          <CustomButton
+            title="Back Home"
+            onPress={() => {
+              setSuccess(false);
+              router.push("/(root)/(tabs)/home");
+            }}
+            className="mt-5"
+          />
+        </View>
+      </ReactNativeModal>
     </>
   );
 };
