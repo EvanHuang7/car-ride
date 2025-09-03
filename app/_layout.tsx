9724e09bfa64c6409b7da6d48c75ca140ccd79b3
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-02 20:28:44 -0600

    hide the warning message in mobile device by using LogBox.ignoreLogs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,10 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { LogBox } from "react-native";
 import "react-native-reanimated";
+
+LogBox.ignoreLogs(["Clerk:"]);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
